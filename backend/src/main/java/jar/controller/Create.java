29da22eb6930d80daa7a9e8fffa8e29de13f6c11
Sai Krishna Jadhav <saikrishnajadhav2006@gmail.com>
2026-08-19 13:23:31 +0530

    Update Create.java

--- backend/src/main/java/jar/controller/Create.java
+++ backend/src/main/java/jar/controller/Create.java
@@ -1,43 +1,21 @@
 package jar.controller;
 
-import java.util.HashMap;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import jar.model.Student;
-import jar.repo.StudentRepo;
-
+import jar.service.Dservice;
 
 @RestController
 @RequestMapping("/api/v1")
 public class Create {
 
     @Autowired
-    StudentRepo db;
+    private Dservice service;
 
     @PostMapping
     public Map<Object, Object> create(@RequestBody Student d) {
-
-        Map<Object, Object> res = new HashMap<>();
-
-        Student s = new Student();
-        s.setName(d.getName());
-        s.setEmail(d.getEmail());
-        s.setIp(d.getIp());
-
-        db.save(s);
-
-        res.put("msg", "Student Added Successfully");
-        res.put("status", 201);
-        res.put("data", s);
-
-        return res;
+        return service.createStudent(d);
     }
-    
-    
 }
